1403d587cf086d6fc2e564fcc81186a66c0c5f7b
Galaxy_2Alex <galaxy2alex@gmail.com>
2014-05-15 17:10:50 +0200

    And that's a block! Yay!

--- src/main/java/info/ppservers/ac/blocks/BlockHandler.java
+++ src/main/java/info/ppservers/ac/blocks/BlockHandler.java
@@ -13,20 +13,21 @@ public class BlockHandler {
     public static Block furnace;
     public static Block furnaceBurning;
     public static Block elementalLog;
-    public static Block elementalLeaves;
-    public static Block elementalPlanks;
+    // public static Block elementalLeaves;
+    // public static Block elementalPlanks;
     
 
     public static void init(){
         furnace = new AlchFurnace(false).setCreativeTab(AlchemicalCombination.ACTab);
         furnaceBurning = new AlchFurnace(true).setLightLevel(0.875F);
+        elementalLog = new ElementalLog().setCreativeTab(AlchemicalCombination.ACTab);
     }
 
     public static void registerBlocks(){
         GameRegistry.registerBlock(furnace,Info.FURNACE_NAME);
         GameRegistry.registerBlock(elementalLog, Info.ELEMENTALLOG_NAME);
-        GameRegistry.registerBlock(elementalLeaves, Info.ELEMENTALLEAVES_NAME);
-        GameRegistry.registerBlock(elementalPlanks, Info.ELEMENTALPLANKS_NAME);
+        // GameRegistry.registerBlock(elementalLeaves, Info.ELEMENTALLEAVES_NAME);
+        // GameRegistry.registerBlock(elementalPlanks, Info.ELEMENTALPLANKS_NAME);
     }
 
     public static void registerTileEntities(){
