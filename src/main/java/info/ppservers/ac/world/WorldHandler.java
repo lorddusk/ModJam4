77e9ee63b1d8cc3fcb7311de8d89ceff20fca530
Galaxy_2Alex <galaxy2alex@gmail.com>
2014-05-16 12:16:36 +0200

    World Handler

--- src/main/java/info/ppservers/ac/world/WorldHandler.java
+++ src/main/java/info/ppservers/ac/world/WorldHandler.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import cpw.mods.fml.common.IWorldGenerator;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class WorldHandler implements IWorldGenerator{
 
@@ -12,6 +13,13 @@ public class WorldHandler implements IWorldGenerator{
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		
+	}
+	
+	public static void registerWorld() {
+		IWorldGenerator generator = null;
+		int modGenerationWeight = 0;
+		
+		GameRegistry.registerWorldGenerator(generator, modGenerationWeight);
 	}
 
 }
